7a03d350aeb1f94bec96ae3d6bfbdc9ddf6a5ec6
lujig <lujig@pku.edu.cn>
2018-05-18 16:33:54 +0800

    update

--- df_assist.py
+++ df_assist.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import numpy as np
 import os,ld
 import numpy.fft as fft
--- lddm.py
+++ lddm.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import numpy as np
 import numpy.ma as ma
 import numpy.fft as fft
